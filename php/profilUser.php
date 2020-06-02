919a8db5021fac03389f34c4cb04a07fd6759155
Cogal <rio.mathieu20@gmail.com>
2020-06-02 21:51:41 +0200

    ajout page connection avec page profil

--- php/profilUser.php
+++ php/profilUser.php
@@ -1,9 +1,39 @@
 <!DOCTYPE html>
-<html>
-    <head>
 
-    </head>
-    <body>
-        
-    </body>
+<html>
+
+        <head>
+
+            <link rel="stylesheet" href="../CSS/profilUser.css">
+            <title>Profil User</title>
+
+        </head>
+
+        <body>
+
+            <div class="content">
+                <div class="userName">
+                    <h1><?php
+                     $username = " Username";
+                      echo "Username :" . $username 
+                      ?></h1>
+                </div>
+             <br>
+                <div class="infoUser">
+                    <h2><?php 
+                    $naissance = "2003";
+                    $villeScolariser = "Sulniac";
+
+                    echo "Née en : " . $naissance;
+                    echo "<br>";
+                    echo "Scolarisée à : " . $villeScolariser;
+                    ?> </h2>
+                </div>
+                <br>
+                <button type="button" class="retourBtn" onclick="window.location.href = '../index.php'">Retourner vers l'acceuil</button>
+
+            </div>
+
+        </body>
+
 </html>
